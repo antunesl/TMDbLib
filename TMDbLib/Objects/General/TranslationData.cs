5e5b6dc21a6a81a900fcea6b2f2c7af01a67ee15
Luís <lr.antunes@gmail.com>
2020-04-22 11:59:54 +0100

    Create TranslationData.cs

---
+++ TMDbLib/Objects/General/TranslationData.cs
@@ -0,0 +1,10 @@
+namespace TMDbLib.Objects.General
+{
+    public class TranslationData {
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Overview { get; set; }
+        public string Homepage { get; set; }
+        public string Tagline { get; set; }
+    }
+}
